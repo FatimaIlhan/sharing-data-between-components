7bd506782a53495ba703e54f03a20518a6a03259
Fatima <fatimahnoor05@gmail.com>
2022-09-01 21:00:14 +0200

    chenge television to tv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sharing-data-between-components';
-  currentItem= 'Television';
+  currentItem= 'TV';
   items = ['item1', 'item2', 'item3', 'item4'];
 
   addItem(newItem: string) {
